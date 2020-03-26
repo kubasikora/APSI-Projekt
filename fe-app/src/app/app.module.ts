293c7add29f7942aa30187c42acf0e9ac7921cf5
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2020-03-26 16:28:33 +0100

    added core module with basic components and login module for login/registration pages/components + basic 404 routing

--- fe-app/src/app/app.module.ts
+++ fe-app/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from "./core/core.module";
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
