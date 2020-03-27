bbd64f9274abfff6d76b7da420d476bab41cfe32
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2020-03-27 14:30:15 +0100

    added bootstrap and created basic app layout

--- fe-app/src/app/app.module.ts
+++ fe-app/src/app/app.module.ts
@@ -1,19 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
 import { CoreModule } from "./core/core.module";
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { AlertModule } from "ngx-bootstrap/alert";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    CoreModule
-  ],
+  declarations: [AppComponent],
+  imports: [AlertModule.forRoot(), BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
