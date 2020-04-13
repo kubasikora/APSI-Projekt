2e25d31f5092067f21811264ed4406e1da6adec3
Julia Klos <julia.klos.stud@pw.edu.pl>
2020-04-13 17:21:55 +0200

    ....

--- fe-app/src/components/RegistrationForm.vue
+++ fe-app/src/components/RegistrationForm.vue
@@ -4,20 +4,23 @@
       label="Imię"
       name="name"
       type="text"
+      @input="setName"
     />
     <v-text-field
       label="Nazwisko"
       name="lastName"
       type="text"
+      @input="setLastName"
     />
       <v-text-field
       label="Numer telefonu"
       name="phone"
       type="text"
+      @input="setPhone"
       append-outer-icon="mdi-phone"
     />
     <v-text-field
-      v-model="email"
+      v-model="userData.email"
       label="Email"
       name="email"
       type="text"
@@ -25,7 +28,7 @@
       append-outer-icon="mdi-email-outline"
     />
     <v-text-field
-            v-model="password1"
+            :value="userData.password"
             :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
             :rules="[rules.required, rules.min]"
             :type="show1 ? 'text' : 'password'"
@@ -33,6 +36,7 @@
             label="Hasło"
             counter
             @click:append="show1 = !show1"
+            @input="setPassword"
           ></v-text-field>
     <v-text-field
             v-model="password2"
@@ -45,13 +49,12 @@
             @click:append="show2 = !show2"
           ></v-text-field>
 
-           <v-radio-group>
+           <v-radio-group row>
           <v-radio
             v-for="r in roles"
             :key="r"
             :label="`${r}`"
-            :value="r"
-             v-model="role"
+            @mouseup="setRole(r)"
           ></v-radio>
            </v-radio-group>
   </v-form>
@@ -59,25 +62,43 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { namespace } from "vuex-class";
 
-@Component({
-  components: {
-  }
-})
+import RegistrationData from "../models/RegistrationData";
+const register = namespace("Registration");
+@Component
 export default class RegistrationForm extends Vue {
+    @register.State
+    public userData: RegistrationData
+
+    @register.Action
+    public setName: (newName: String) => void
+
+    @register.Action
+    public setLastName: (newLastName: String) => void
+
+    @register.Action
+    public setPhone: (newPhone: String) => void
+
+    @register.Action
+    public setEmail: (newEmail: String) => void
+
+    @register.Action
+    public setRole: (newRole: String) => void
+
+    @register.Action
+    public setPassword: (newPassword: String) => void
      data () {
       return {
-        email: '',
         show1: false,
         show2: false,
-        password1: '',
         password2: '',
         role: '',
         roles: ["wolontariusz", "odbiorca pomocy"],
         rules: {
           required: (value: string) => !! value || 'Wymagane',
           min: (v: string) =>  v.length >= 8 || 'Hasło musi zawierać min. 8 znaków',
-          passwordMatch: ( v: string, password1: string, checkPasswords: Boolean) => !(!(v == password1) && checkPasswords) || "Hasła muszą byc takie same"
+          passwordMatch: ( v: string, checkPasswords: Boolean) => !(!(v == this.userData.password) && checkPasswords) || "Hasła muszą byc takie same"
         },
       }
     }
--- fe-app/src/store/index.ts
+++ fe-app/src/store/index.ts
@@ -1,12 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import Login from "@/store/modules/login";
-
+import Registration from "@/store/modules/registration"
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
-    Login
+    Login,
+    Registration
   }
 });
 
