7c16cb40d043df9b08666f1a17bc4151283bd389
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2020-04-14 14:57:37 +0200

    fixed cors issues

--- backend/django/bookmarks/bookmarks/settings.py
+++ backend/django/bookmarks/bookmarks/settings.py
@@ -40,10 +40,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'account',
     'orders',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +126,11 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+# CORS 
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+]
