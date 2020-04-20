dc32622eb760fdcf83f9b787a41cb7bc4776a9b1
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2020-04-20 15:24:45 +0200

    wydzielenie komponentów

---
+++ fe-app/src/components/AccountActionsPanel.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-card class="action-card">
+    <v-form>
+      <v-card-title>Akcje</v-card-title>
+      <v-divider />
+      <v-row>
+        <v-col cols="6">
+          <h3 class="action-label">Zmień hasło:</h3>
+        </v-col>
+        <v-col cols="6">
+          <v-spacer />
+          <v-btn color="success" class="alert-button button-col">Zmień</v-btn>
+        </v-col>
+      </v-row>
+      <v-divider />
+      <v-row>
+        <v-col cols="6">
+          <h3 class="action-label">Usuń konto:</h3>
+        </v-col>
+        <v-col cols="6">
+          <v-spacer />
+          <v-btn color="#f00" class="alert-button button-col">Usuń</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import BaselineLayout from "@/layouts/BaselineLayout.vue";
+
+@Component
+export default class AccountActionsPanel extends Vue {}
+</script>
+
+<style scoped>
+.alert-button {
+  color: white;
+}
+
+.action-label {
+  margin-left: 1rem;
+}
+
+.action-card {
+  margin: 2rem 0;
+}
+
+.button-col {
+  float: right;
+  margin: 0 10px;
+}
+</style>
---
+++ fe-app/src/components/AccountDataPanel.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-form>
+      <v-card-title>Moje dane</v-card-title>
+      <v-divider />
+      <v-card-text>
+        <v-text-field label="Imię" color="primary"></v-text-field>
+        <v-text-field label="Nazwisko" color="primary"></v-text-field>
+        <v-text-field label="Adres" color="primary" loading></v-text-field>
+        <v-text-field label="Miasto" color="primary" loading></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="primary">Anuluj</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn color="success">Zapisz</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import BaselineLayout from "@/layouts/BaselineLayout.vue";
+
+@Component
+export default class AccountDataPanel extends Vue {}
+</script>
+
+<style scoped>
+</style>
