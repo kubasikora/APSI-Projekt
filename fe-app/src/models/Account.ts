56274f3aad4638296d0e6f831bc194626559d1d8
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2020-04-24 10:41:45 +0200

    reading role & username from cookies

---
+++ fe-app/src/models/Account.ts
@@ -0,0 +1,9 @@
+export default class LoginCredentials {
+    constructor(email: String, password: String){
+        this.email = email;
+        this.password = password;
+    }
+  
+    readonly email: String;
+    readonly password: String;
+  };
---
+++ fe-app/src/services/AccountService.ts
@@ -0,0 +1,12 @@
+import LoginCredentials from "@/models/LoginCredentials";
+import LoginResponse from "@/models/LoginResponse";
+
+interface APILoginCredentials {
+  username: String;
+  password: String;
+}
+
+
+export default class AccountService {
+
+}
