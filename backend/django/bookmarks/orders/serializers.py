90b2c455577ecd44f5d3c0a5871029735bb67a09
pawelzakieta <pawelzakieta97@gmail.com>
2020-04-24 09:29:53 +0200

    Order ID returned

--- backend/django/bookmarks/orders/serializers.py
+++ backend/django/bookmarks/orders/serializers.py
@@ -8,5 +8,5 @@ class OrderSerializer(serializers.ModelSerializer):
     boomer = serializers.ReadOnlyField(source='boomer.user.username')
     class Meta:
         model = Order
-        fields = ('description', 'boomer', 'volunteer')
+        fields = ('id', 'description', 'boomer', 'volunteer')
         #fields = ('__all__', 'boomer')
