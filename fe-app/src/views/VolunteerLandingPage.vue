f05a397063859d91084061c7a5820cc12f3810c0
Robert <01121305@pw.edu.pl>
2020-04-25 15:38:31 +0200

    static data for now

--- fe-app/src/views/VolunteerLandingPage.vue
+++ fe-app/src/views/VolunteerLandingPage.vue
@@ -9,7 +9,7 @@
     <v-divider></v-divider>
 
     <div class="task-list">
-      <ListOfTasks v-bind:tasks="boomers"></ListOfTasks>
+      <ListOfTasks v-bind:tasks="tasksData"></ListOfTasks>
     </div>
   </BaselineLayout>
 </template>
@@ -18,8 +18,7 @@
 import { Component, Vue } from "vue-property-decorator";
 import BaselineLayout from "@/layouts/BaselineLayout.vue";
 import ListOfTasks from "@/components/ListOfTasks.vue";
-// import { Boomer } from "src/models/TaskInterface.vue";
-// import ExampleTasksData from 'src/models/TaskInterface.vue';
+import { Boomer } from '../models/TaskInterface';
 
 @Component({
   components: {
@@ -28,11 +27,51 @@ import ListOfTasks from "@/components/ListOfTasks.vue";
   }
 })
 export default class VolunteerLandingPageView extends Vue {
-
-  // TODO: ogarnac ladowanie przykladowych danych
-  // boomers() {
-  //   return ExampleTasksData;
-  // }
+  // TODO: wywalic statyczna tablice i zamienic na dane z API
+  tasksData: Boomer[] = [
+    {
+        name: "Robert Wojtas",
+        residence: {
+            street: "Ułańska",
+            homeNumber: 12
+        },
+        phoneNumber: "606115394",
+        due: "15-04-2020",
+        listID: 1
+    },
+    {
+        name: "Paweł Żakieta",
+        residence: {
+            street: "Kartuzy",
+            homeNumber: 69,
+            aptNumber: 69
+        },
+        phoneNumber: "696969696",
+        due: "19-04-2020",
+        listID: 2
+    },
+    {
+        name: "Jakub Sikora",
+        residence: {
+            street: "Arka Gdynia",
+            homeNumber: 10
+        },
+        phoneNumber: "111115394",
+        due: "15-07-2020",
+        listID: 3
+    },
+    {
+        name: "Krzysiu Bednarski",
+        residence: {
+            street: "Warszawa",
+            homeNumber: 420,
+            aptNumber: 69
+        },
+        phoneNumber: "696969696",
+        due: "01-11-2020",
+        listID: 4
+    }
+];
 
   // TODO: do zrobienia jak bedzie dostepne API
   getList(id: number) {
