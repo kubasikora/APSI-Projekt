0478886f1a91bfda6800fe8b0a8d07bf04cb81b4
Robert <01121305@pw.edu.pl>
2020-04-25 15:37:46 +0200

    new styles

--- fe-app/src/components/ListOfTasks.vue
+++ fe-app/src/components/ListOfTasks.vue
@@ -7,7 +7,7 @@
         <span class="icon">
           <v-icon>mdi-account</v-icon>
         </span>
-        <span>{{ task.name }}</span>
+        <span class="boomer-name"><strong>{{ task.name }}</strong></span>
       </v-expansion-panel-header>
 
       <v-expansion-panel-content>
@@ -15,21 +15,21 @@
           <v-row>
             <v-col>
               <v-card class="pa-2" tile>
-                Adres:
+                <strong>Adres: </strong>
                 <span>{{ task.residence.street + ' ' + task.residence.homeNumber }}</span>
                 <span v-if="task.residence.aptNumber">{{ '/' + task.residence.aptNumber }}</span>
               </v-card>
             </v-col>
             <v-col>
-              <v-card class="pa-2" tile>Numer telefonu: {{ task.phoneNumber }}</v-card>
+              <v-card class="pa-2" tile><strong>Numer telefonu:</strong> {{ task.phoneNumber }}</v-card>
             </v-col>
           </v-row>
           <v-row>
             <v-col>
-              <v-card class="pa-2" tile>Data dostarczenia: {{ task.due }}</v-card>
+              <v-card class="pa-2" tile><strong>Data dostarczenia:</strong> {{ task.due }}</v-card>
             </v-col>
             <v-col>
-              <v-btn v-on:click="getList(task.listID)">Zobacz listę</v-btn>
+              <v-btn color="accent" v-on:click="getList(task.listID)">Zobacz listę</v-btn>
             </v-col>
           </v-row>
         </v-container>
@@ -52,8 +52,31 @@ export default class ListOfTasks extends Vue {
 }
 </script>
 
-<style>
+<style scoped>
 .icon {
   max-width: 5%;
 }
+
+.mdi::before {
+  color: #5b5f97;
+}  
+
+.boomer-name {
+  font-size: 1.2rem;
+  color: #5b5f97;
+}
+
+.v-expansion-panel--active > .v-expansion-panel-header {
+  background-color: #b8b8d1;  
+  color: #fff;
+}
+
+.v-expansion-panel--active > .v-expansion-panel-header .mdi::before {
+  color: #fff;
+}
+
+.v-expansion-panel--active > .v-expansion-panel-header .boomer-name {
+  color: #fff;
+}
+
 </style>
