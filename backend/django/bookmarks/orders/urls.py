894b9397ac7ac59e4421e87a8a42cea3f32e8c95
kbednars <krzy.bednarski97@gmail.com>
2020-06-20 21:43:01 +0200

    Poprawa do update produktów

--- backend/django/bookmarks/orders/urls.py
+++ backend/django/bookmarks/orders/urls.py
@@ -1,13 +1,12 @@
 from django.conf.urls import url
-from .views import OrderList, OrderDetail, OrderInRadius, AssignedOrders, CreatedOrders, ProductList, ProductListDetail, AssignOrder
+from .views import OrderList, OrderDetail, OrderInRadius, AssignedOrders, CreatedOrders, ProductList, ProductListDetail
 from django.urls import path
 
 
 urlpatterns = [path('', OrderList.as_view()),
                path('<int:pk>', OrderDetail.as_view()),
-               path('<orderPK>/assignOrder', AssignOrder.as_view()),
                path('productList_<orderPK>', ProductList.as_view()),
-               path('productList_<orderPK>/<int:pk>', ProductListDetail.as_view()),
+               path('productList_<orderPK>/<pk>', ProductListDetail.as_view()),
                path('orderInRadius_<coordx>_<coordy>_<dist>/', OrderInRadius.as_view()),
                path('createdOrders', CreatedOrders.as_view()),
                path('assignedOrders', AssignedOrders.as_view())]
