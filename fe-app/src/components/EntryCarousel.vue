01c8fa1af9f0c04c0bb8a8b03e2d25f166d4318a
kubasikora <283418@pw.edu.pl>
2020-06-21 16:46:22 +0200

    ekran startowy

--- fe-app/src/components/EntryCarousel.vue
+++ fe-app/src/components/EntryCarousel.vue
@@ -1,11 +1,14 @@
 <template>
   <v-carousel cycle height="250" hide-delimiter-background show-arrows-on-hover>
-    <v-carousel-item v-for="(slide, i) in slides" :key="i">
-      <v-sheet :color="colors[i]" height="100%">
+    <v-carousel-item 
+      v-for="(item, i) in srcs" :key="i"
+      :src="item"
+      >
+      <!-- <v-sheet :color="colors[i]" height="100%">
         <v-row class="fill-height" align="center" justify="center">
           <div class="display-3">{{ slide }}</div>
         </v-row>
-      </v-sheet>
+      </v-sheet> -->
     </v-carousel-item>
   </v-carousel>
 </template>
@@ -15,6 +18,7 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class EntryCarousel extends Vue {
+  private srcs: Array<String> = ['baba.jpg', 'biedronka-senior.jpg', 'aktualnosci_senior-hero.jpg', 'harold.jpg', 'pomoc.jpg']
   private colors: Array<String> = ['indigo', 'warning', 'pink darken-2', 'red lighten-1', 'deep-purple accent-4'];
   private slides: Array<String> = ['Pierwszy', 'Drugi', 'Trzeci', 'Czwarty', 'Piąty'];
 };
--- fe-app/src/views/GuestLandingPage.vue
+++ fe-app/src/views/GuestLandingPage.vue
@@ -3,41 +3,63 @@
     <v-container>
       <v-row>
         <v-col>
-          <EntryCarousel/>
+          <EntryCarousel />
         </v-col>
       </v-row>
       <v-row>
         <v-col cols="4">
           <v-card class="info-card">
             <v-card-text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus placerat lectus. Phasellus fermentum nisl sem. Vestibulum sed dui pulvinar urna euismod tincidunt non id mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in velit tempor, commodo ligula at, fringilla lorem. Nulla nec semper justo. Proin posuere nisl augue, sit amet tincidunt justo efficitur vel. Nunc sed ultrices ipsum, quis tincidunt enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut pretium cursus lectus et posuere. Morbi leo lorem, auctor rhoncus nisl sit amet, pulvinar efficitur neque.
-              Duis ac fringilla ex. Praesent vestibulum viverra quam, nec auctor ligula molestie eget. Nulla commodo lacus tellus, a placerat eros sollicitudin eu. Mauris rhoncus dapibus diam, et feugiat sapien efficitur in. Proin sed tempor urna, vel rhoncus lectus. Ut ut justo nisi. Aliquam non tortor ipsum. Nunc ac iaculis mi. Sed mollis libero elit, ut molestie est viverra lacinia. Phasellus convallis velit in ornare tempor. Quisque pellentesque lobortis commodo. Nulla molestie orci at velit consequat, ac malesuada lectus gravida. Curabitur sodales consectetur diam, sit amet consequat urna volutpat at. Curabitur iaculis, mauris a venenatis lobortis, ipsum velit pulvinar sapien, interdum pellentesque magna tortor sit amet diam. Quisque consectetur rutrum sem, vitae dignissim libero.
-              <v-spacer/>
+              <strong>JESTEŚMY TUTAJ BY POMÓC</strong>
+              <br />Nasza aplikacja o nazwie Helpado jest systemem umożliwiającym udzielenie pomocy osobom potrzebującym poprzez wykonanie dla nich zakupów.
+              Pomysł na aplikację narodził się z analizy potrzeb starszego i chorego społeczeństwa w obliczu pandemii,
+              aby ułatwić im codzienne funkcjonowanie i zapewnić bezpieczeństwo w tych trudnych czasach.
+              <v-spacer />
             </v-card-text>
           </v-card>
           <v-card>
             <v-card-text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus placerat lectus. Phasellus fermentum nisl sem. Vestibulum sed dui pulvinar urna euismod tincidunt non id mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in velit tempor, commodo ligula at, fringilla lorem. Nulla nec semper justo. Proin posuere nisl augue, sit amet tincidunt justo efficitur vel. Nunc sed ultrices ipsum, quis tincidunt enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut pretium cursus lectus et posuere. Morbi leo lorem, auctor rhoncus nisl sit amet, pulvinar efficitur neque.
-              Duis ac fringilla ex. Praesent vestibulum viverra quam, nec auctor ligula molestie eget. Nulla commodo lacus tellus, a placerat eros sollicitudin eu. Mauris rhoncus dapibus diam, et feugiat sapien efficitur in. Proin sed tempor urna, vel rhoncus lectus. Ut ut justo nisi. Aliquam non tortor ipsum. Nunc ac iaculis mi. Sed mollis libero elit, ut molestie est viverra lacinia. Phasellus convallis velit in ornare tempor. Quisque pellentesque lobortis commodo. Nulla molestie orci at velit consequat, ac malesuada lectus gravida. Curabitur sodales consectetur diam, sit amet consequat urna volutpat at. Curabitur iaculis, mauris a venenatis lobortis, ipsum velit pulvinar sapien, interdum pellentesque magna tortor sit amet diam. Quisque consectetur rutrum sem, vitae dignissim libero.
-              <v-spacer/>
+              <strong>Opinie szczęśliwych użytkowników:</strong>
+              <br />
+              <i>-"To jest wspaniałe!!! Nareszczie nie muszę chodzić sama po papier toaletowy w tej okropnej maseczce, w której czuje jaki mam okropny oddech. DZIĘKUJĘ HELPADO!!"</i>
+              <br />
+              <br />
+              <i>-"Bardzo mi się podoba ten interfejs. Wszystko mam pod ręką i tam gdzie trzeba. W prosty sposób mogę znaleźć potrzebujących ludzi w okolicy. Poznaje ich adresy. A kiedy już usną to kradnę z ich domów co się da"</i>
+              <br />
+              <br />
+              <i>-"Moja babcia nareszcie może otrzymywać pomoc od kogoś, kto nie jest mną. W końcu trochę wolnego. HELPADU FOR THE WIN!!!"</i>
+              <br />
+              <v-spacer />
             </v-card-text>
           </v-card>
-
         </v-col>
         <v-col cols="8">
           <v-card class="info-card">
             <v-card-text>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla faucibus placerat lectus. Phasellus fermentum nisl sem. Vestibulum sed dui pulvinar urna euismod tincidunt non id mauris. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in velit tempor, commodo ligula at, fringilla lorem. Nulla nec semper justo. Proin posuere nisl augue, sit amet tincidunt justo efficitur vel. Nunc sed ultrices ipsum, quis tincidunt enim. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Ut pretium cursus lectus et posuere. Morbi leo lorem, auctor rhoncus nisl sit amet, pulvinar efficitur neque.
-              Duis ac fringilla ex. Praesent vestibulum viverra quam, nec auctor ligula molestie eget. Nulla commodo lacus tellus, a placerat eros sollicitudin eu. Mauris rhoncus dapibus diam, et feugiat sapien efficitur in. Proin sed tempor urna, vel rhoncus lectus. Ut ut justo nisi. Aliquam non tortor ipsum. Nunc ac iaculis mi. Sed mollis libero elit, ut molestie est viverra lacinia. Phasellus convallis velit in ornare tempor. Quisque pellentesque lobortis commodo. Nulla molestie orci at velit consequat, ac malesuada lectus gravida. Curabitur sodales consectetur diam, sit amet consequat urna volutpat at. Curabitur iaculis, mauris a venenatis lobortis, ipsum velit pulvinar sapien, interdum pellentesque magna tortor sit amet diam. Quisque consectetur rutrum sem, vitae dignissim libero.
-              Sed feugiat vestibulum dolor, at ornare mi iaculis ac. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque faucibus feugiat dapibus. Aliquam erat volutpat. Maecenas volutpat commodo justo quis condimentum. Nullam rhoncus congue dapibus. Phasellus porta molestie risus, ac varius ex faucibus et. Donec sagittis lorem leo. Suspendisse consectetur id magna in blandit. Morbi id lobortis tellus. Aliquam risus velit, sollicitudin eu sodales nec, hendrerit ut neque. Nulla luctus laoreet quam id efficitur.
-              Vivamus euismod interdum risus, rhoncus tempus dui fermentum vitae. Vivamus cursus nisi metus, at feugiat ex semper mollis. Praesent ligula diam, vestibulum nec rutrum sit amet, commodo ac metus. Nunc metus neque, consequat non tempus at, vehicula id nunc. Duis mi enim, placerat et elit sed, ornare pharetra enim. Donec pulvinar magna ac ultricies pretium. Ut malesuada fringilla elit mollis dapibus. Etiam posuere placerat luctus. Nam porta a dui sed tempus. Curabitur vel vehicula ligula.
-              Duis ultrices sapien ac nisl lobortis condimentum. Pellentesque elementum imperdiet nulla, sed pellentesque nunc malesuada eu. Aliquam malesuada eros a velit bibendum, eget venenatis arcu hendrerit. Donec viverra, turpis et consectetur maximus, odio orci gravida diam, ut suscipit diam dolor ac nibh. Aenean porta placerat enim, id gravida justo maximus tincidunt. Cras consectetur porttitor erat a sodales. Vivamus nisi metus, facilisis at mi ut, rhoncus vehicula eros. Morbi non pulvinar diam.
-              Suspendisse rhoncus elit diam, ut gravida urna pulvinar ut. Fusce quis vestibulum dui, quis accumsan eros. Etiam et fermentum diam. Nunc accumsan feugiat consectetur. Ut leo est, maximus vel finibus at, volutpat nec libero. Duis sit amet blandit sapien. Aliquam a magna tempus, lobortis mauris ac, congue risus. Suspendisse vel erat sit amet odio porta dictum.
-              In ac lorem ut turpis interdum ultricies. Sed ut commodo nulla, ac fermentum elit. Curabitur tincidunt, nibh sed elementum dictum, dui augue consequat leo, id rhoncus mauris eros in magna. Ut ultricies sit amet metus nec dictum. Quisque tincidunt pellentesque aliquet. Mauris id fringilla enim, ut ultricies orci. Quisque at nunc eu enim dapibus suscipit maximus vel tortor. Ut vehicula, sapien vel varius maximus, odio augue egestas nisl, eu mollis risus felis at leo. Vivamus ornare convallis faucibus. Suspendisse luctus convallis erat sed accumsan. Etiam nec feugiat leo.
-              Quisque sem elit, convallis et enim a, facilisis faucibus risus. Nullam at rhoncus turpis. Aliquam a maximus magna. Duis euismod vulputate velit at consectetur. Sed semper, sapien et dignissim fringilla, risus libero sagittis ex, eget malesuada ex risus vitae risus. Etiam at dapibus nisi. Etiam ac augue faucibus ante ullamcorper eleifend. Pellentesque vitae turpis mauris. Vivamus porttitor tempus tristique. Sed eros risus, iaculis ut sollicitudin sit amet, semper nec metus. Nulla ac erat congue, auctor mi vehicula, dignissim arcu. Maecenas pretium enim vitae eros condimentum porta. Maecenas luctus blandit ullamcorper. Maecenas vestibulum orci ut orci bibendum semper. Praesent dignissim vel magna eget porta. Maecenas in lorem euismod, consequat tellus a, mattis ligula.
-              Ut id diam lacus. Donec aliquam at quam ac tristique. Integer tellus massa, scelerisque a faucibus in, accumsan id ante. Maecenas ex erat, venenatis id nulla a, ultricies consectetur augue. Curabitur risus mauris, dapibus in varius non, tristique maximus urna. Nunc tristique nulla nibh, at fringilla tellus lobortis id. Nullam a commodo tellus, sit amet pellentesque tortor. Sed pulvinar felis quis risus cursus, nec consectetur ante efficitur. Vestibulum rhoncus tortor fermentum, pellentesque ipsum ac, laoreet nisl. Morbi id blandit dui. Sed faucibus vel nulla eu tristique. Nunc ligula tortor, pretium nec magna in, pretium sagittis nisi. Quisque justo ante, viverra mollis mi id, eleifend commodo tellus.
-              Aliquam erat volutpat. Etiam molestie vestibulum justo, sed bibendum quam gravida vel. Integer suscipit eros nec mauris eleifend, ac fringilla enim consequat. Praesent porta auctor varius. Donec at suscipit lacus, eget dapibus dolor. Quisque ac purus non arcu condimentum porttitor et id ante. Sed tincidunt id neque eu ornare. Curabitur ac venenatis nulla. Nam malesuada ornare augue. Mauris egestas dignissim consectetur.
-              <v-spacer/>
+              Pewnego razu żył sobie Boomer.
+              <br />Boomer był stary i niedołężny.
+              <br />Bolały go stopy.
+              <br />Bolała go głowa.
+              <br />Bolały go plecy.
+              <br />Bolała go sytuacja polityczna w kraju bez pieniędzy i z PISem u władzy.
+              <br />Nie radził sobie z tym wszystkim.
+              <br />Pewnego dnia stała się tragedia.
+              <br />Do kraju biednego Boomera przybył straszny wirus co nosił koronę.
+              <br />Nazywał sie Koronawirus.
+              <br />Władza państwa zamknęła biednego Boomera w mieszkaniu przez co
+              nie mógł sobie sam zrobić zakupów przez co często miał brudno w domu i chodził głodny.
+              <br />Biedne było życie Boomera.
+              <br />Aż tu nagle.
+              <br />Pewnego razu.
+              <br />Do drzwi Boomera zapukał Wolontariusz.
+              <br />Silny, umięśniony czarny mężczyzna gej z torbą zakupów dla biednego staruszka.
+              <br />Był to sąsiad Boomera, który widział, że nie łatwo mu jest teraz, więc postanowił mu pomóc.
+              <br />Boomer tak się ucieszył, że w końcu ktoś mu pomógł.
+              <br />Widząc radość na twarzy Boomera, Wolontariusz postanowił skontaktować się z ekspertami branży IT,
+              <br />aby oni właśnie, wykorzystując swoje doświadczenie i więdzę, stworzyli aplikację, która umożliwi Boomerom wyszukiwać wolontariuszy.
+              <br />W ten właśnie sposób powstała aplikacja <strong><i>HELPADO</i></strong>, która niesie pomoc uciśnionym i potrzebującym.
+              <br />
+              <v-spacer />
             </v-card-text>
           </v-card>
         </v-col>
@@ -57,9 +79,7 @@ import EntryCarousel from "@/components/EntryCarousel.vue";
     EntryCarousel
   }
 })
-export default class GuestLandingPageView extends Vue {
-
-};
+export default class GuestLandingPageView extends Vue {}
 </script>
 
 <style scoped>
